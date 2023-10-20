c7dce9cd737fd7b1c492adf391bc7a196d4a214c
MMGGHD <tldmseogus@naver.com>
2023-10-20 11:41:20 +0900

    [김대현] 1140

--- src/main/java/com/example/kakao/cart/Cart.java
+++ src/main/java/com/example/kakao/cart/Cart.java
@@ -4,9 +4,39 @@ import lombok.*;
 
 import javax.persistence.*;
 
+import com.example.kakao.product.Product;
+
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name = "cart_tb")
 public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    // 판매자 이름
+    private String sellerName;
+
+    // 상품 테이블
+    private Product product;
+
+    // 수량
+    private Integer quantity;
+
+    // 배송비
+    private Integer deliveryFee;
+
+    // // 예정금액
+    // private Integer estimatedPrivce;
+
+    public Cart(Integer id, String sellerName, Product product, Integer quantity, Integer deliveryFee) {
+        this.id = id;
+        this.sellerName = sellerName;
+        this.product = product;
+        this.quantity = quantity;
+        this.deliveryFee = deliveryFee;
+    }
+
 }
--- src/main/java/com/example/kakao/product/Product.java
+++ src/main/java/com/example/kakao/product/Product.java
@@ -2,6 +2,9 @@ package com.example.kakao.product;
 
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.*;
 
 @Getter
@@ -9,4 +12,32 @@ import javax.persistence.*;
 @Entity
 @Table(name = "product_tb")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer Id;
+    // 썸네일
+    @Column(length = 500)
+    private String thumbnail;
+    // 판매자 이름
+    @Column(length = 100, nullable = false)
+    private String seller;
+    // 상품 타이틀
+    @Column(length = 100, nullable = false)
+    private String productTitle;
+    @Column(length = 500)
+    // 할인전 가격
+    private Integer beforeDiscount;
+    // 할인율
+    private Integer discountreat;
+
+    @Builder
+    public Product(Integer id, String thumbnail, String seller, String productTitle, Integer beforeDiscount,
+            Integer discountreat) {
+        Id = id;
+        this.thumbnail = thumbnail;
+        this.seller = seller;
+        this.productTitle = productTitle;
+        this.beforeDiscount = beforeDiscount;
+        this.discountreat = discountreat;
+    }
 }
