30ba9b1c8617289876c52ef8732d434a90193f96
MMGGHD <tldmseogus@naver.com>
2023-10-20 10:53:05 +0900

    [231020 1055]

--- src/main/java/com/example/kakao/order/Order.java
+++ src/main/java/com/example/kakao/order/Order.java
@@ -8,29 +8,13 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-import com.example.kakao.user.User;
-
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-@Table(name="order_tb")
+@Table(name = "order_tb")
 public class Order {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    private User user;
-
-    @Builder
-    public Order(int id, User user) {
-        this.id = id;
-        this.user = user;
-    }
 }
